2debd144eb4fc05c820ff8db4718ba13ee0efc1e
katasu <katasu9923@gmail.com>
2019-04-26 00:20:10 +0900

    とりあえずレスポンシブ対応

--- pages/about.vue
+++ pages/about.vue
@@ -44,70 +44,190 @@ export default {
 }
 
 .main_wrapper {
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  margin: auto;
-  height: 50%;
+  height: 100%;
   width: 100%;
+  padding: 8vmin;
+}
+
+@media screen and (min-height:320px) and (max-height:415px) and (max-width:737px){
+
+/* iPhone5, iPhone6~8 & plus */
+
+.main_wrapper {
+  height: 100%;
+  width: 100%;
+  padding: 16vmin;
+  overflow: scroll;
 }
 
 .sub_wrapper {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
-  width: calc(100vw - 4vh * 2);
-  height: 38vh;
-}
-
-.aboutus {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
-  width: 200px;
-  height: 100px;
-  display: flex;
-  align-items: center;
-  color: white;
-  font-size: 32px;
-  font-weight: 900;
-  z-index: 2;
+    position: relative;
+    height: 96vmin;
+    margin-top: 16vmin;
 }
 
 .img_wrapper {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  width: 38vh;
-  height: 38vh;
-  z-index: 1;
-  background-color: orangered;
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 96vmin;
+    height: 96vmin;
+    z-index: 1;
+    background-color: orangered;
+}
+
+.aboutus {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    width: 200px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    color: white;
+    font-size: 16vmin;
+    font-weight: 900;
+    white-space: nowrap;
+    z-index: 2;
 }
 
 .logo {
-    position: absolute;
-    top: 4vh;
-    left: 4vh;
-    margin: auto;
-    width: calc(100vw - 4vh * 2 - 38vh);
-    height: 3vh;
+    width: calc(100vw - 16vmin * 2 - 96vmin);
+    height: 32vmin;
     display: flex;
     align-items: center;
     justify-content: center;
 }
 
 .mikanLogo {
-    height: calc(1.5vw + 1.5vh);
-    margin: 0 4px 0 0;
+    width: 37%;
+    margin-right: 6%;
 }
 
 .tunaLogo {
-    height: calc(1.5vw + 1.5vh);
+    width: 57%;
+}
+
+}
+
+@media screen and (min-height:320px) and (max-height:415px) and (min-width:811px) {
+
+/* iPhoneX , XS Max */
+
+.main_wrapper {
+  height: 100%;
+  width: 100%;
+  padding: 16vmin;
+  overflow: scroll;
+}
+
+.sub_wrapper {
+    position: relative;
+    height: 120vmin;
+    margin-top: 16vmin;
+}
+
+.img_wrapper {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 120vmin;
+    height: 120vmin;
+    z-index: 1;
+    background-color: orangered;
+}
+
+.aboutus {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    width: 200px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    color: white;
+    font-size: 16vmin;
+    font-weight: 900;
+    white-space: nowrap;
+    z-index: 2;
+}
+
+.logo {
+    width: calc(100vw - 16vmin * 2 - 120vmin);
+    height: 32vmin;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.mikanLogo {
+    width: 37%;
+    margin-right: 6%;
+}
+
+.tunaLogo {
+    width: 57%;
+}
+
+}
+
+@media screen and (min-height:568px) {
+
+  /* 縦画面 */
+
+.sub_wrapper {
+    position: relative;
+    height: 64vmin;
+    margin-top: 8vmin;
+}
+
+.aboutus {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    width: 200px;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    color: white;
+    font-size: 6vmin;
+    font-weight: 900;
+    white-space: nowrap;
+    z-index: 2;
+}
+
+.img_wrapper {
+    position: absolute;
+    right: 0;
+    width: 64vmin;
+    height: 64vmin;
+    z-index: 1;
+    background-color: orangered;
+}
+
+.logo {
+    width: calc(100vw - 8vmin * 2 - 64vmin);
+    height: 16vmin;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.mikanLogo {
+    width: 37%;
+    margin-right: 6%;
+}
+
+.tunaLogo {
+    width: 57%;
+}
+
 }
 
 
